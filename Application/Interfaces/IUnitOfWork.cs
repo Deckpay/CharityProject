deebe74651af8a46da81f091e7264e38a7b4be49
Bence Papp <bencesalsa@gmail.com>
2026-03-06 20:48:30 +0100

    generikus crud műveletek hopzzáadva biztonságos mentes unitofworkel

---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    // Az IDisposable biztosítja, hogy az adatbázis-kapcsolat lezáruljon, ha végeztünk
+    public interface IUnitOfWork : IDisposable
+    {
+        // Elérhetővé tesszük a konkrét repository-kat
+        IGenericRepository<Product> Products { get; }
+        IGenericRepository<User> Users { get; }
+        IGenericRepository<ProductCategory> Categories { get; }
+        IGenericRepository<County> Counties { get; }
+
+        // Ez a metódus menti el az összes változtatást (Tranzakció kezelés)
+        Task<int> CompleteAsync();
+    }
+}
