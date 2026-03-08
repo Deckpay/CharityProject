c5e82e5aaaf21c3bf69d24ec64daa0a16e9abfdb
Bence Papp <bencesalsa@gmail.com>
2026-03-08 19:05:42 +0100

    elkészült a product oldal és a hozzá tartozó crud műveletek

---
+++ Application/DTOs/ProductDto.cs
@@ -0,0 +1,30 @@
+﻿/*
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+*/
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "A termék neve kötelező")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A termék leírása kötelező")]
+        public string? ProductDescription { get; set; } = string.Empty;
+
+        public string? ImagePath { get; set; }
+
+        [Required(ErrorMessage = "Kategória választása kötelező")]
+        public int ProductCategoryId { get; set; }
+
+        [Required(ErrorMessage = "Megye választása kötelező")]
+        public int CountyId { get; set; }
+
+        // A DonorId-t nem a DTO-ban kérjük be, azt majd a bejelentkezett 
+        // felhasználóból vesszük ki a szerveren!
+    }
+}
---
+++ Application/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+
+//using Microsoft.AspNetCore.Components.Forms;
+
+namespace Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<bool> CreateProductAsync(ProductDto productDto);
+        Task<IEnumerable<ProductDto>> GetProductsAsync();
+        Task DeleteProductAsync(int id);
+    }
+}
